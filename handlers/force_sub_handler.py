3fa6c602f5ea654b2400a7affd655fd322a26dec
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2021-06-16 12:40:49 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -37,7 +37,7 @@ async def handle_force_sub(bot, cmd):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/trvpn).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
